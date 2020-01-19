79759769b9cd21d2d7c1a2e6abbea986cb2a72c0
Ilya Glushchuk <musicguy675@gmail.com>
2020-01-19 21:38:59 +1000

    Add image loading method

--- MarvelRX/MarvelRX/API/API.swift
+++ MarvelRX/MarvelRX/API/API.swift
@@ -11,12 +11,32 @@ import RxSwift
 import RxCocoa
 
 final class API {
-    private let session = URLSession.shared
+    private let session: URLSession = .shared
+    private let cache: URLCache = .shared
 
     var characters: Observable<CharacterDataWrapper> {
         let request = URLRequest(url: APIConstants.charactersURL)
         return session.rx.data(request: request).map {
             try JSONDecoder().decode(CharacterDataWrapper.self, from: $0)
+        }
+    }
+
+    func loadImage(with url: URL) -> Observable<UIImage?> {
+        let request = URLRequest(url: url)
+
+        if let cachedResponse = cache.cachedResponse(for: request) {
+            return Observable<UIImage?>.create { observer -> Disposable in
+                observer.on(.next(UIImage(data: cachedResponse.data)))
+                observer.on(.completed)
+
+                return Disposables.create()
+            }
+        }
+
+        return session.rx.response(request: request).map { [weak self] (response, data) -> UIImage? in
+            self?.cache.storeCachedResponse(CachedURLResponse(response: response, data: data), for: request)
+
+            return UIImage(data: data)
         }
     }
 }
